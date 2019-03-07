6bb00740477b24dba627f2a97fcd793d7ae5a634
jfjfjbjb <697451yr@163.com>
2019-03-07 15:42:07 +0800

    首页白屏优化

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import Routes from './router';
 
 // import App from './components/app';
+import './app.css';
 import './index.scss';
 import './demo';
+
+// 处理首屏loading
+// 100ms后开启，10s后关闭
+let preLoader = document.querySelector('#pre-loader');
+if(preLoader) {
+    setTimeout(() => {
+        preLoader.style.display = 'block';
+    }, 100);
+    setTimeout(() => {
+        preLoader.style.display = 'none';
+    }, 10000);
+}
 
 render(
     <Router routes={Routes} history={browserHistory}/>, document.querySelector('#app')
 );
+
